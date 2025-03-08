fd40d989017ec42081cc6f5b9809763fff20d629
Henrique <henriquegarde@gmail.com>
2025-03-08 17:58:50 +0000

    Finished hotbar, choosing item and shotgun

--- scripts/gui/Inventory/shotgun.gd
+++ scripts/gui/Inventory/shotgun.gd
@@ -3,9 +3,6 @@ class_name ShotgunItem extends InventoryItem
 @export var shotgun_class: PackedScene = preload("res://scenes/weapons/shotgun.tscn")
 var shotgun
 
-func use(player) -> void:
-	print("sheesh")
-
 func new_instance() -> Node2D:
 	shotgun = shotgun_class.instantiate()
 	return shotgun
--- scripts/player.gd
+++ scripts/player.gd
@@ -12,7 +12,6 @@ const JUMP_VELOCITY = -300.0
 var current_weapon: Node2D = null
 
 func _ready():
-	inventory.use_item.connect(use_item)
 	inventory.show_item.connect(show_item)
 
 func _physics_process(delta: float) -> void:
@@ -53,15 +52,11 @@ func _on_hurt_box_area_entered(area):
 	if area.has_method("collect") and not inventory.isFull():
 		area.collect(inventory)
 
-
-func use_item(item: InventoryItem) -> void:
-	item.use(self)
 	
 func show_item(item: InventoryItem) -> void:
 	if not current_weapon == null:
 		weapon_holder.get_child(0).queue_free()
 		current_weapon = null
-		
 	if item and item is ShotgunItem:
 		current_weapon = item.new_instance()
 		weapon_holder.add_child(current_weapon)
