18de2a60d2803072d068c80d8c2fd2064ed62b2f
Henrique Gardé <up202108725@up.pt>
2025-03-18 19:02:49 +0000

    Added mask

---
+++ scripts/mask.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+const SPEED = 60
+
+var direction = 1
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
