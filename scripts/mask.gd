857e8192618ddb6cac2d460d07bc5816e0b27a89
Henrique <henriquegarde@gmail.com>
2025-03-22 16:02:39 +0000

    Stun done

--- scripts/mask.gd
+++ scripts/mask.gd
@@ -6,6 +6,11 @@ var direction = 1
 var health_points = 1
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var timer: Timer = $Timer
+
+func _ready():
+	timer.wait_time = 2
 
 func get_damage(damage: int):
 	health_points -= damage
@@ -14,3 +19,11 @@ func get_damage(damage: int):
 
 func die():
 	queue_free()
+
+func stun():
+	animation_player.pause()
+	timer.start()
+	
+
+func _on_timer_timeout() -> void:
+	animation_player.play()
--- scripts/weapons/paper.gd
+++ scripts/weapons/paper.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @export var speed = 200
-@export var damage = 15
+
 var velocity = Vector2.ZERO  # Initial velocity
 func _ready():
 	await get_tree().create_timer(3).timeout
@@ -21,6 +21,6 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	#if body.is_in_group("enemies"):
-	#	body.get_damage(damage)
-		queue_free()
+	if body.is_in_group("enemies"):
+		body.stun()
+	queue_free()
