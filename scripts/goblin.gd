94ad0d10055be4b432b942e4b3c07dee2d54766b
Henrique <henriquegarde@gmail.com>
2025-03-24 14:21:13 +0000

    Fixed goblin movement

--- scripts/goblin.gd
+++ scripts/goblin.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 60
+@export var speed: float = 60
+@export var wait: float = 2.0
 
 var direction = 1
 var health_points = 1
@@ -8,25 +9,27 @@ var is_stunned = false
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var stun_timer: Timer = $Stun_Timer
+@onready var movement_timer: Timer = $Movement_Timer  # Add a new timer in the scene
 
 func _ready():
 	stun_timer.wait_time = 2
-	start_movement_cycle()
+	movement_timer.wait_time = wait  # Set movement timer interval
+	movement_timer.start()  # Start the timer to begin movement cycle
 
 func _physics_process(delta: float) -> void:
 	if not is_stunned:
-		velocity.x = SPEED * direction
+		velocity.x = speed * direction
 	else:
 		velocity.x = 0  # Stop movement when stunned
 
 	move_and_slide()
 
-func start_movement_cycle():
-	while true:
-		await get_tree().create_timer(2.0).timeout  # Wait 2 seconds
-		if not is_stunned:
-			direction *= -1
-			animated_sprite.flip_h = !animated_sprite.flip_h
+func _on_movement_timer_timeout() -> void:
+	if is_stunned:
+		return  # Do nothing if stunned
+	direction *= -1
+	animated_sprite.flip_h = !animated_sprite.flip_h
+	movement_timer.start()  # Restart the movement timer
 
 func get_damage(damage: int):
 	health_points -= damage
@@ -37,12 +40,13 @@ func die():
 	Global.kills += 1
 	queue_free()
 
-
 func stun():
 	is_stunned = true
 	animated_sprite.pause()
 	stun_timer.start()
+	movement_timer.paused = true  # Pause movement timer when stunned
 
 func _on_stun_timer_timeout() -> void:
 	is_stunned = false
 	animated_sprite.play()
+	movement_timer.paused = false  # Resume movement timer after stun
