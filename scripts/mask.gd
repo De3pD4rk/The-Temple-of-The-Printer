425298fbfa8f22b4a70f7b23cb3e5ba37e58ae07
Henrique <henriquegarde@gmail.com>
2025-03-24 16:04:30 +0000

    Enemies places

--- scripts/mask.gd
+++ scripts/mask.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 60
+@export var speed: float = 60
+@export var flip_x: bool = false  # Exported variable to control Y flip
 
 var direction = 1
 var health_points = 1
@@ -11,6 +12,7 @@ var health_points = 1
 
 func _ready():
 	timer.wait_time = 2
+	animated_sprite.flip_h = flip_x  # Apply the flip setting at start
 
 func get_damage(damage: int):
 	health_points -= damage
@@ -24,7 +26,6 @@ func die():
 func stun():
 	animation_player.pause()
 	timer.start()
-	
 
 func _on_timer_timeout() -> void:
 	animation_player.play()
